a5cf4adc7c1b7c9e3dd4bc9384cd030100221986
sajan basnet <sajanbasnet75@gmail.com>
2017-10-09 09:53:55 +0545

    first commit
    designs

---
+++ src/hrms/MySqlConnect.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hrms;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LORDsajan
+ */
+public class MySqlConnect {
+
+    public static Connection conn;
+
+    public static Connection connectDb() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_db", "root", "");
+            System.out.println("Database Connected Successfully !");
+            return conn;
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+            JOptionPane.showMessageDialog(null, "Database cannot be connected !");
+            System.exit(0);
+        }
+        return null;
+    }
+
+}
